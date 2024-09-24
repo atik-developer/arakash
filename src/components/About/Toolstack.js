a474bf593556cee4debd6d173fe9927c8e63e02a
AR AKASH <arakash968786@gmail.com>
2024-09-25 02:53:37 +0600

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -12,19 +12,29 @@ function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
+      <div className="flex justify-center w-full items-center">
         <SiMacos />
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+      <div className="flex justify-center w-full items-center">
         <SiVisualstudiocode />
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+      <div className="flex justify-center w-full items-center">
         <SiPostman />
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+      <div className="flex justify-center w-full items-center">
         <SiSlack />
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+      <div className="flex justify-center w-full items-center">
         <SiVercel />
+        </div>
       </Col>
     </Row>
   );
