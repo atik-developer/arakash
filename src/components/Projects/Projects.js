155741c50045b7de5aa9183d4b1e37d60850aa86
akash100a <arakash968786@gmail.com>
2024-09-25 00:37:46 +0600

    u

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,10 +25,10 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
+              title="Orebi Ecommarce"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink=""
-              demoLink="chatify"
+              demoLink="https://orebi4.vercel.app/"
             />
           </Col>
 
