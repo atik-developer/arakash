2915025390a6feed26a7d36411f53c25a4a0ed8c
akash100a <arakash968786@gmail.com>
2024-10-06 12:03:26 +0600

    u

--- app/component/layers/GenCard.jsx
+++ app/component/layers/GenCard.jsx
@@ -4,13 +4,13 @@ import Image from 'next/image'
 
 const Toolcard = ({percen,title}) => {
     return (
-        <div className='bg-[#ffffff05] box-border rounded-2xl w-44 h-44 border-2 dark:border-[#ffffff1f] center flex-col gap-4'>
+        <div className='dark:bg-[#ffffff05] box-border rounded-2xl w-44 h-44 border-2 dark:border-[#ffffff1f] center flex-col gap-4'>
             <div className="rounded-full w-28 h-28 border-4 dark:border-[#ffffff1f] relative after:absolute after:left-1/2 after:top-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:w-full after:h-full after:border-4 box-border after:rounded-full after:border-[#78cc6d] center text-[#fff]">
                 <div className="center">
-                    <h3 className='font-medium text-[1.06rem] leading-[529%] text-center text-white/[0.9]'>{percen}%</h3>
+                    <h3 className='font-medium text-[1.06rem] leading-[529%] text-center text-[#0c0c0c] dark:text-white/[0.9]'>{percen}%</h3>
                 </div>
             </div>
-            <h3 className='font-normal text-sm leading-[114%] text-center text-white/[0.9]'>{title}</h3>
+            <h3 className='font-normal text-sm leading-[114%] text-center text-[#0c0c0c] dark:text-white/[0.9]'>{title}</h3>
         </div>
     )
 }
