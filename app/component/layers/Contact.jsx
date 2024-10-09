460d08a97cd439b7f74df6703c6df5e1f62897c2
akash100a <arakash968786@gmail.com>
2024-10-10 01:16:57 +0600

    up

--- app/component/layers/Contact.jsx
+++ app/component/layers/Contact.jsx
@@ -5,6 +5,8 @@ import Image from 'next/image'
 import Phone from '../../../public/icon/phone.png'
 import Mail from '../../../public/icon/mail.png'
 import Address from '../../../public/icon/address.png'
+import Link from 'next/link'
+import Send2 from '../../../public/icon/send2.png'
 
 
 const Contact = () => {
@@ -46,12 +48,24 @@ const Contact = () => {
                 </div>
             </div>
             <div className="map mb-[90px] w-[780px] h-[450px]">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1825.8776111855225!2d90.38719611156844!3d23.756106697395374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8a426199b0d%3A0x6a2c655d06c88ec9!2sFarmgate%2C%20Dhaka%201205!5e0!3m2!1sen!2sbd!4v1728291119158!5m2!1sen!2sbd" className='w-full h-full' style={{ border: "0", allowfullscreen: "", loading: "lazy" }} referrerPolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1825.8776111855225!2d90.38719611156844!3d23.756106697395374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8a426199b0d%3A0x6a2c655d06c88ec9!2sFarmgate%2C%20Dhaka%201205!5e0!3m2!1sen!2sbd!4v1728291119158!5m2!1sen!2sbd" className='w-full h-full dark:mix-blend-luminosity' style={{ border: "0", allowfullscreen: "", loading: "lazy" }} referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
-            <div className="contactform"></div>
+            <div className="contactform">
+                <h2 className='mb-[46px] font-medium text-[2.38rem] leading-[130%] dark:text-[#fff] text-[#0c0c0c]'>Let's make your project brilliant!</h2>
+                <form action="">
+                    <div className="flex items-center gap-8">
+                        <input className='py-2 px-4 font-normal outline-none bg-transparent border dark:border-[#ffffff1f] text-base dark:text-[#999] text-[#0c0c0c] rounded-xl w-[374px] h-[50px]' type="text" name="" id="" placeholder='Full Name' />
+                        <input className='py-2 px-4 font-normal outline-none bg-transparent border dark:border-[#ffffff1f] text-base dark:text-[#999] text-[#0c0c0c] rounded-xl w-96 h-12' type="email" name="" id="" placeholder='Email' />
+                    </div>
+                    <textarea className='mt-8 rounded-xl w-[48.69rem] h-[120px] py-2 px-4 outline-none bg-transparent border dark:border-[#ffffff1f] dark:text-[#999] text-[#0c0c0c]' name="" id=""></textarea>
+
+                    <Link className='pb-10 flex gap-2 items-baseline justify-start font-semibold text-sm uppercase text-center text-white/[0.9] mt-8' href='#'>Send Message <Image src={Send2} /></Link>
+                </form>
+            </div>
         </div>
     )
 }
 
 export default Contact
 
+// mix-blend-mode: luminosity;
