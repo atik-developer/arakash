b3265f773f2b7325dcac79f210edb017981fc59a
akash100a <arakash968786@gmail.com>
2024-11-12 01:50:39 +0600

    meta

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,28 @@ import Home from "./component/pages/Home";
 import LeftSide from "./component/layers/LeftSide";
 
 
+// export async function generateMetadata() {
+//   let brand = "ATIQUR RAHMAN AKASH"
+//   let mainurl = "https://arakash.vercel.app"
+//   let keywords = ["arakash", "ATIQUR RAHMAN AKASH"]
+//   let description = `Next.js Developer | Specializing in Fast & Scalable Web Apps..
+// I am Atiqur Rahman Akash, a dedicated Next.js Developer with over 2.5 years of experience in designing and implementing fast, responsive, and scalable web applications. I specialize in creating innovative and efficient web solutions tailored to meet client needs.I possess a strong command of front-end technologies, including HTML, CSS, Bootstrap, Tailwind, JavaScript, jQuery, React.js, Redux, and Next.js. My skill set also includes crafting smooth animations using GSAP and collaborating on UI/UX designs with tools like Figma and Adobe XD. By leveraging GitHub for version control, I ensure seamless collaboration and effective code management across all my projects.I am committed to delivering high-quality, interactive user experiences. My problem-solving mindset and keen attention to detail empower me to tackle challenges efficiently and consistently meet project goals.I invite you to explore my portfolio to view examples of my work:https://arakash.vercel.app/#skills For inquiries, please contact me at arakash.developer@gmail.com.`
+//   return {
+//     title: brand,
+//     description: description,
+//     keywords: keywords,
+//     openGraph: {
+//       type: 'website',
+//       url: mainurl,
+//       title: brand,
+//       description: description,
+//       // images: newdata.images,
+//       site_name: 'arakash',
+//       locale: 'en_US',
+//     }
+//   }
+// }
+
 export default function Root() {
 
   return (
