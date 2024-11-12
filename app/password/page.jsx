dd2ad37cd955aef37b581e24f59c0a6f66760ee3
akash100a <arakash968786@gmail.com>
2024-11-12 13:24:58 +0600

    a

---
+++ app/password/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Container from '../component/layers/Container'
+
+const page = () => {
+  return (
+    <Container>
+        <div className='my-5'>
+            <h1>Gmail </h1>
+            <p>gmail : arakash.figma@gmail.com</p>
+            <p>password : arakash556600aa5</p>
+        </div>
+        <div className='my-5'>
+            <h1>Figma </h1>
+            <p>gmail : arakash.figma@gmail.com</p>
+            <p>password : akashfigma556600</p>
+        </div>
+
+    </Container>
+  )
+}
+
+export default page
